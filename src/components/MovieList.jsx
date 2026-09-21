81c4b900c51cd57d7b66f2758446d4229b967c1d
jelalqaiumi <qaiumi@hotmail.com>
2026-09-21 10:44:17 +0200

    lagt till komponent för filmlistan.

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,15 @@
+﻿import MovieCard from "./MovieCard";
+
+export default function MovieList({ movies, onEdit, onUpload }) {
+  if (movies.length === 0) {
+    return <p className="empty">Inga filmer att visa än.</p>;
+  }
+
+  return (
+    <div className="grid">
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} onEdit={onEdit} onUpload={onUpload} />
+      ))}
+    </div>
+  );
+}
